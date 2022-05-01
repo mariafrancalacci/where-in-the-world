6efb3f19442b458926105fdbac222d4cba905624
mariafrancalacci <malu.cartier@outlook.com>
2022-05-01 23:39:42 +0100

    Creation code of id country

---
+++ pages/country/[id].tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Link from "next/link";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { Country } from "../../types";
+import { GetStaticPaths } from "next";
+
+export interface DetailsProps {
+  data: Country;
+  res: Country;
+}
+
+const Details = ({ data }: DetailsProps) => {
+  return (
+    <>
+      <Header />
+      <div>
+        <div className="px-4 pt-12 sm:px-24 h-screen">
+          <Link href="/">
+            <button className="shadow-lg bg-white px-7 py-2 flex justify-between items-center">
+              <FontAwesomeIcon className="mr-4" icon={faArrowLeft} />
+              <span>Back</span>
+            </button>
+          </Link>
+
+          <div className="flex flex-col pt-20 sm:items-start ">
+            <Image
+              src="/project-1.jpg"
+              width="300"
+              height="200"
+              className="flex-non object-cover w-full"
+            />
+            <div className="pb-10">
+              <div className="flex flex-col gap-2">
+                <h1 className="font-bold pt-10 mb-4 text-lg">{data.name}</h1>
+                <span>Native Name: {data.nativeName}</span>
+                <span>Population: {data.population}</span>
+                <span>Region: {data.region}</span>
+                <span>Sub Region:{data.subregion}</span>
+                <span>Capital:{data.capital}</span>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <span>Top Level Domain:{data.nativeName}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Details;
+
+// https://restcountries.com/v2/alpha/pe
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await fetch(`https://restcountries.com/v2/all`);
+  const data = await res.json();
+
+  return {
+    paths: data.map((country: Country) => {
+      return { params: { alpha3Code: String(country.alpha3Code) } };
+    }),
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({
+  params,
+}: {
+  params: { alpha3Code: string };
+}) => {
+  const res = await fetch(
+    `https://restcountries.com/v2/alpha/${params.alpha3Code}`
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};

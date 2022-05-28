2c727183ce815f08c514e3c94fde3f6af43608bb
mariafrancalacci <malu.cartier@outlook.com>
2022-05-28 21:06:16 +0100

    updated app.tsx file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,5 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Header from "../components/Header/Header";
-import SearchBar from "../components/SearchBar/SearchBar";
-import Card from "../components/Card/Card";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
